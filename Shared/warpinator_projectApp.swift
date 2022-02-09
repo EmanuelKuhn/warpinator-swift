40f1e990cb9f332d2491997e767779d1dc47fec2
Emanuel Kuhn <kuhn.emanuel@gmail.com>
2022-02-09 11:45:56 +0100

    Initial Commit

---
+++ Shared/warpinator_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  warpinator_projectApp.swift
+//  Shared
+//
+//  Created by Emanuel on 09/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct warpinator_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
